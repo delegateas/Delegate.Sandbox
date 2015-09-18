f8629f331ccde91e061f04ecac08057346ddbeba
Ramón Soto Mathiesen <rsm@delegate.dk>
2015-09-18 10:56:26 +0200

    Pretty much ready to a v.2.0 :=)

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,10 @@
+### 1.5.0.0 - July 30 2015
+* Major code refactoring (less code, more awesome)
+* Fixed issue with No code optmization in Release mode
+* Fixed issue with Unit tests. See Delegate.Sandbox.Tests project
+* Added support for nested sandboxes. Ex: sandbox{ return sandbox{ return 42 } }
+* Thanks to nested sandboxes, it's now possible to ensure that a libary is 100%
+  IO Effect safe thanks to FSharp.Compiler.Services and a Post-Build F# script
+
 ### 1.0.0.0 - July 30 2015
 * Initial release
--- src/Delegate.Sandbox/AssemblyInfo.fs
+++ src/Delegate.Sandbox/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Delegate.Sandbox")>]
 [<assembly: AssemblyProductAttribute("Delegate.Sandbox")>]
 [<assembly: AssemblyDescriptionAttribute("Delegate.Sandbox is library that provides I/O side-effects safe code by using a sandbox computation expression.")>]
-[<assembly: AssemblyVersionAttribute("1.0.0.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.0.0")>]
+[<assembly: AssemblyVersionAttribute("1.5.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.5.0.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.0.0"
+    let [<Literal>] Version = "1.5.0.0"
